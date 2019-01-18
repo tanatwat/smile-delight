e451cec048643e3382f5c4adfd148784d43cddca
Tanatwat Wattanasakul <scomusicrusted@gmail.com>
2019-01-18 17:20:20 +0700

    add messenger SDK

--- js/app.js
+++ js/app.js
@@ -80,3 +80,24 @@ window.document.addEventListener("DOMContentLoaded", function() {
 });
 
 Vue.component('vue-slick', require('./components/Slick.vue').default);
+
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: '315338012438169',
+    autoLogAppEvents: true,
+    xfbml: true,
+    version: 'v3.2'
+  });
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
